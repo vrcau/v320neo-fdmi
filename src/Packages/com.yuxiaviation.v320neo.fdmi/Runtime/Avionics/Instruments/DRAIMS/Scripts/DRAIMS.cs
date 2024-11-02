8e1a03a0ded9886735e0968675fe8b03e5964722
Misaka-L <lipww1234@foxmail.com>
2024-11-02 12:48:14 +0800

    feat(draims): send input to page

--- src/Packages/com.yuxiaviation.v320neo.fdmi/Runtime/Avionics/Instruments/DRAIMS/Scripts/DRAIMS.cs
+++ src/Packages/com.yuxiaviation.v320neo.fdmi/Runtime/Avionics/Instruments/DRAIMS/Scripts/DRAIMS.cs
@@ -39,39 +39,50 @@ namespace VRChatAerospaceUniversity.V320.Avionics.Instruments.DRAIMS.Scripts {
 
     #region SelectionKeys
 
-        [PublicAPI] public void _OnLeft1Pressed() { }
-        [PublicAPI] public void _OnLeft2Pressed() { }
-        [PublicAPI] public void _OnLeft3Pressed() { }
-        [PublicAPI] public void _OnLeft4Pressed() { }
-        [PublicAPI] public void _OnRight1Pressed() { }
-        [PublicAPI] public void _OnRight2Pressed() { }
-        [PublicAPI] public void _OnRight3Pressed() { }
-        [PublicAPI] public void _OnRight4Pressed() { }
+        [PublicAPI] public void _OnLeft1Pressed() => SendInputToPage(DRAIMSKeyType.SelectionLeft1);
+        [PublicAPI] public void _OnLeft2Pressed() => SendInputToPage(DRAIMSKeyType.SelectionLeft2);
+        [PublicAPI] public void _OnLeft3Pressed() => SendInputToPage(DRAIMSKeyType.SelectionLeft3);
+        [PublicAPI] public void _OnLeft4Pressed() => SendInputToPage(DRAIMSKeyType.SelectionLeft4);
+        [PublicAPI] public void _OnRight1Pressed() => SendInputToPage(DRAIMSKeyType.SelectionRight1);
+        [PublicAPI] public void _OnRight2Pressed() => SendInputToPage(DRAIMSKeyType.SelectionRight2);
+        [PublicAPI] public void _OnRight3Pressed() => SendInputToPage(DRAIMSKeyType.SelectionRight3);
+        [PublicAPI] public void _OnRight4Pressed() => SendInputToPage(DRAIMSKeyType.SelectionRight4);
 
     #endregion
 
     #region Keyboard
 
-        [PublicAPI] public void _OnNumber1Pressed() { }
-        [PublicAPI] public void _OnNumber2Pressed() { }
-        [PublicAPI] public void _OnNumber3Pressed() { }
-        [PublicAPI] public void _OnNumber4Pressed() { }
-        [PublicAPI] public void _OnNumber5Pressed() { }
-        [PublicAPI] public void _OnNumber6Pressed() { }
-        [PublicAPI] public void _OnNumber7Pressed() { }
-        [PublicAPI] public void _OnNumber8Pressed() { }
-        [PublicAPI] public void _OnNumber9Pressed() { }
-        [PublicAPI] public void _OnNumber0Pressed() { }
+        [PublicAPI] public void _OnNumber1Pressed() => SendInputToPage(DRAIMSKeyType.Number1);
+        [PublicAPI] public void _OnNumber2Pressed() => SendInputToPage(DRAIMSKeyType.Number2);
+        [PublicAPI] public void _OnNumber3Pressed() => SendInputToPage(DRAIMSKeyType.Number3);
+        [PublicAPI] public void _OnNumber4Pressed() => SendInputToPage(DRAIMSKeyType.Number4);
+        [PublicAPI] public void _OnNumber5Pressed() => SendInputToPage(DRAIMSKeyType.Number5);
+        [PublicAPI] public void _OnNumber6Pressed() => SendInputToPage(DRAIMSKeyType.Number6);
+        [PublicAPI] public void _OnNumber7Pressed() => SendInputToPage(DRAIMSKeyType.Number7);
+        [PublicAPI] public void _OnNumber8Pressed() => SendInputToPage(DRAIMSKeyType.Number8);
+        [PublicAPI] public void _OnNumber9Pressed() => SendInputToPage(DRAIMSKeyType.Number9);
+        [PublicAPI] public void _OnNumber0Pressed() => SendInputToPage(DRAIMSKeyType.Number0);
 
-        [PublicAPI] public void _OnDotPressed() { }
-        [PublicAPI] public void _OnClearPressed() { }
+        [PublicAPI] public void _OnDotPressed() => SendInputToPage(DRAIMSKeyType.Dot);
+        [PublicAPI] public void _OnClearPressed() => SendInputToPage(DRAIMSKeyType.Clear);
 
     #endregion
 
-        [PublicAPI] public void _OnLeftGenericKeyPressed() { }
-        [PublicAPI] public void _OnRightGenericKeyPressed() { }
-        [PublicAPI] public void _OnUpKeyPressed() { }
-        [PublicAPI] public void _OnDownKeyPressed() { }
+        [PublicAPI] public void _OnLeftGenericKeyPressed() => SendInputToPage(DRAIMSKeyType.GenericLeft);
+        [PublicAPI] public void _OnRightGenericKeyPressed() => SendInputToPage(DRAIMSKeyType.GenericRight);
+        [PublicAPI] public void _OnUpKeyPressed() => SendInputToPage(DRAIMSKeyType.Up);
+        [PublicAPI] public void _OnDownKeyPressed() => SendInputToPage(DRAIMSKeyType.Down);
+
+        private void SendInputToPage(DRAIMSKeyType keyType) {
+            var pageObject = _pageRouter._GetObjectByPath(_pageRouter.CurrentPath);
+
+            var page = pageObject.GetComponentInChildren<DraimsPageBase>(true);
+            if (!page) {
+                return;
+            }
+
+            page._OnKeyPressed(keyType);
+        }
 
     #endregion
 

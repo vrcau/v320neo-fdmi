c4a9292da292dcdb6a02b697b543af335be8fff0
Misaka-L <lipww1234@foxmail.com>
2024-11-03 15:48:37 +0800

    feat(draims): input freq

--- src/Packages/com.yuxiaviation.v320neo.fdmi/Runtime/Avionics/Instruments/DRAIMS/Scripts/Components/RadioComponent.cs
+++ src/Packages/com.yuxiaviation.v320neo.fdmi/Runtime/Avionics/Instruments/DRAIMS/Scripts/Components/RadioComponent.cs
@@ -1,11 +1,11 @@
-﻿using JetBrains.Annotations;
+﻿using System.Diagnostics.CodeAnalysis;
+using JetBrains.Annotations;
 using tech.gyoku.FDMi.core;
 using TMPro;
 using UdonSharp;
 using UnityEngine;
 
-namespace VRChatAerospaceUniversity.V320.Avionics.Instruments.DRAIMS.Scripts.Components
-{
+namespace VRChatAerospaceUniversity.V320.Avionics.Instruments.DRAIMS.Scripts.Components {
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     [AircraftLifecycleReceiver]
     public class RadioComponent : UdonSharpBehaviour {
@@ -24,12 +24,15 @@ namespace VRChatAerospaceUniversity.V320.Avionics.Instruments.DRAIMS.Scripts.Com
         [SerializeField] private TextMeshProUGUI _standByFrequencyText;
 
         [SerializeField] private GameObject _isSelectedIcon;
+        [SerializeField] private TextMeshProUGUI _standByText;
 
-        [SerializeField] private GameObject _editingUnderline;
-
+        [SerializeField] private Color _selectedColor;
+        [SerializeField] private Color _unselectedColor;
+        [SerializeField] private Color _notValidColor;
 
         [PublicAPI]
-        public void _InitComponent(FDMiFloat frequency, FDMiFloat standByFrequency, FDMiBool isReceive, FDMiBool isTransmit, DRAIMS draims) {
+        public void _InitComponent(FDMiFloat frequency, FDMiFloat standByFrequency, FDMiBool isReceive,
+            FDMiBool isTransmit, DRAIMS draims) {
             _frequency = frequency;
             _standByFrequency = standByFrequency;
             _isReceive = isReceive;
@@ -43,28 +46,169 @@ namespace VRChatAerospaceUniversity.V320.Avionics.Instruments.DRAIMS.Scripts.Com
             _OnFrequencyChange();
             _OnReceiveChange();
             _OnTransmitChange();
+
+            _DeSelect();
         }
 
         public void _OnFrequencyChange() {
             _frequencyText.text = _frequency.Data.ToString("000.000");
-            _standByFrequencyText.text = _standByFrequency.Data.ToString("000.000");
+
+            if (_editingTextString == "")
+                _standByFrequencyText.text = _standByFrequency.Data.ToString("000.000");
         }
 
         public void _OnReceiveChange() {
             _receiveIcon.SetActive(_isReceive.Data);
         }
 
-        public void _OnTransmitChange()
-        {
+        public void _OnTransmitChange() {
             _transmitIcon.SetActive(_isTransmit.Data);
         }
 
         public void _SwipeFrequency() {
+            if (_editingTextString.Length >= 3) {
+                _standByFrequency.Data = float.Parse(_editingTextString);
+            }
+
+            if (_editingTextString != "") {
+                _editingTextString = "";
+                SetStandBy();
+            }
+
             // ReSharper disable once SwapViaDeconstruction
             var newStandByFrequency = _frequency.Data;
 
             _frequency.Data = _standByFrequency.Data;
             _standByFrequency.Data = newStandByFrequency;
+        }
+
+        public void _Select() {
+            _isSelectedIcon.SetActive(true);
+            SetStandBy();
+        }
+
+        public void _DeSelect() {
+            _isSelectedIcon.SetActive(false);
+            SetStandBy();
+
+            _editingTextString = "";
+            _standByFrequencyText.color = _unselectedColor;
+        }
+
+        private string _editingTextString = "";
+        private const string _editingPlaceholder = "___.___";
+        private const string _EditingLast3DigitPlaceholder = "___.000";
+        private const string _maxFrequency = "136.975";
+        private const string _minFrequency = "108.000";
+
+        public void _Input(DRAIMSKeyType keyType) {
+            var charToInput = ' ';
+            switch (keyType) {
+                case DRAIMSKeyType.Number0:
+                    charToInput = '0';
+                    break;
+                case DRAIMSKeyType.Number1:
+                    charToInput = '1';
+                    break;
+                case DRAIMSKeyType.Number2:
+                    charToInput = '2';
+                    break;
+                case DRAIMSKeyType.Number3:
+                    charToInput = '3';
+                    break;
+                case DRAIMSKeyType.Number4:
+                    charToInput = '4';
+                    break;
+                case DRAIMSKeyType.Number5:
+                    charToInput = '5';
+                    break;
+                case DRAIMSKeyType.Number6:
+                    charToInput = '6';
+                    break;
+                case DRAIMSKeyType.Number7:
+                    charToInput = '7';
+                    break;
+                case DRAIMSKeyType.Number8:
+                    charToInput = '8';
+                    break;
+                case DRAIMSKeyType.Number9:
+                    charToInput = '9';
+                    break;
+                case DRAIMSKeyType.Clear:
+                    _editingTextString = "";
+                    UpdateEditing();
+                    return;
+            }
+
+            if (charToInput == ' ') return;
+            if (_editingTextString.Length == 3) {
+                _editingTextString += ".";
+            }
+
+            _editingTextString += charToInput;
+
+            UpdateEditing();
+        }
+
+        private void UpdateEditing() {
+            var isInputNotValid = UpdateNotValid();
+
+            if (_editingTextString.Length == 7) {
+                if (!isInputNotValid) {
+                    var newFrequency = float.Parse(_editingTextString);
+                    _standByFrequency.Data = newFrequency;
+                    _editingTextString = "";
+                    SetStandBy();
+
+                    return;
+                }
+
+                _editingTextString = "";
+                SetStandBy();
+            }
+            else if (_editingTextString.Length >= 3) {
+                // ReSharper disable once ReplaceSubstringWithRangeIndexer
+                _standByFrequencyText.text = _editingTextString +
+                                             _EditingLast3DigitPlaceholder.Substring(_editingTextString.Length);
+                return;
+            }
+
+            // ReSharper disable once ReplaceSubstringWithRangeIndexer
+            _standByFrequencyText.text = _editingTextString + _editingPlaceholder.Substring(_editingTextString.Length);
+        }
+
+        [SuppressMessage("ReSharper", "ReplaceSubstringWithRangeIndexer")]
+        private bool UpdateNotValid() {
+            if (_editingTextString == "") {
+                SetStandBy();
+                return false;
+            }
+
+            var minFrequencySub = float.Parse(_minFrequency.Substring(0, _editingTextString.Length));
+            var maxFrequencySub = float.Parse(_maxFrequency.Substring(0, _editingTextString.Length));
+
+            if (float.Parse(_editingTextString) < minFrequencySub ||
+                float.Parse(_editingTextString) > maxFrequencySub) {
+                SetNotValid();
+                return true;
+            }
+
+            SetStandBy();
+            return false;
+        }
+
+        private void SetStandBy() {
+            _standByFrequencyText.text = _standByFrequency.Data.ToString("000.000");
+            _standByFrequencyText.color = _selectedColor;
+            _standByText.color = _unselectedColor;
+            _standByText.text = "STBY";
+        }
+
+        private void SetNotValid() {
+            _standByFrequencyText.text = _standByFrequency.Data.ToString("000.000");
+            _standByFrequencyText.color = _notValidColor;
+            _standByText.color = _notValidColor;
+            _standByText.text = "NOT VALID";
         }
     }
 }
--- src/Packages/com.yuxiaviation.v320neo.fdmi/Runtime/Avionics/Instruments/DRAIMS/Scripts/Pages/DraimsVHFPage.cs
+++ src/Packages/com.yuxiaviation.v320neo.fdmi/Runtime/Avionics/Instruments/DRAIMS/Scripts/Pages/DraimsVHFPage.cs
@@ -9,7 +9,6 @@ namespace VRChatAerospaceUniversity.V320.Avionics.Instruments.DRAIMS.Scripts.Pag
     [AircraftLifecycleReceiver]
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
     public class DraimsVHFPage : DraimsPageBase {
-
         private FDMiBool _isVHF1Receive;
         private FDMiBool _isVHF1Transmit;
 
@@ -33,6 +32,8 @@ namespace VRChatAerospaceUniversity.V320.Avionics.Instruments.DRAIMS.Scripts.Pag
         [SerializeField] private FDMiFloat _vhf2StandByFrequency;
         [SerializeField] private FDMiFloat _vhf3StandByFrequency;
 
+        private RadioComponent _selectComponent;
+
         public override void _FirstInitPage(DRAIMS draims) {
             base._FirstInitPage(draims);
 
@@ -45,9 +46,14 @@ namespace VRChatAerospaceUniversity.V320.Avionics.Instruments.DRAIMS.Scripts.Pag
             _isVHF3Receive = _draims._isVHF3Receive;
             _isVHF3Transmit = _draims._isVHF3Transmit;
 
-            _vhf1Component._InitComponent(_vhf1Frequency, _vhf1StandByFrequency, _isVHF1Receive, _isVHF1Transmit, draims);
-            _vhf2Component._InitComponent(_vhf2Frequency, _vhf2StandByFrequency, _isVHF2Receive, _isVHF2Transmit, draims);
-            _vhf3Component._InitComponent(_vhf3Frequency, _vhf3StandByFrequency, _isVHF3Receive, _isVHF3Transmit, draims);
+            _vhf1Component._InitComponent(_vhf1Frequency, _vhf1StandByFrequency, _isVHF1Receive, _isVHF1Transmit,
+                draims);
+            _vhf2Component._InitComponent(_vhf2Frequency, _vhf2StandByFrequency, _isVHF2Receive, _isVHF2Transmit,
+                draims);
+            _vhf3Component._InitComponent(_vhf3Frequency, _vhf3StandByFrequency, _isVHF3Receive, _isVHF3Transmit,
+                draims);
+
+            SelectComponent(_vhf1Component);
         }
 
         public override void _OnKeyPressed(DRAIMSKeyType keyType) {
@@ -63,7 +69,44 @@ namespace VRChatAerospaceUniversity.V320.Avionics.Instruments.DRAIMS.Scripts.Pag
                 case DRAIMSKeyType.SelectionLeft3:
                     _vhf3Component._SwipeFrequency();
                     break;
+                case DRAIMSKeyType.SelectionRight1:
+                    SelectComponent(_vhf1Component);
+                    break;
+                case DRAIMSKeyType.SelectionRight2:
+                    SelectComponent(_vhf2Component);
+                    break;
+                case DRAIMSKeyType.SelectionRight3:
+                    SelectComponent(_vhf3Component);
+                    break;
+                case DRAIMSKeyType.Number0:
+                case DRAIMSKeyType.Number1:
+                case DRAIMSKeyType.Number2:
+                case DRAIMSKeyType.Number3:
+                case DRAIMSKeyType.Number4:
+                case DRAIMSKeyType.Number5:
+                case DRAIMSKeyType.Number6:
+                case DRAIMSKeyType.Number7:
+                case DRAIMSKeyType.Number8:
+                case DRAIMSKeyType.Number9:
+                case DRAIMSKeyType.Clear:
+                    _selectComponent._Input(keyType);
+                    break;
             }
+        }
+
+        private void SelectComponent(RadioComponent component) {
+            DeSelectAll();
+
+            _selectComponent = component;
+            _selectComponent._Select();
+        }
+
+        private void DeSelectAll() {
+            _selectComponent = null;
+
+            _vhf1Component._DeSelect();
+            _vhf2Component._DeSelect();
+            _vhf3Component._DeSelect();
         }
     }
 }
